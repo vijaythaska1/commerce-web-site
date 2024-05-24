0d7e826d9bc24ac880d7bf01eb4c1c22ec4d4677
Vijay Sharma <you@example.com>
2024-05-24 15:31:53 +0530

    issue no rejole

--- src/axios/APIServices.js
+++ src/axios/APIServices.js
@@ -1,15 +1,18 @@
 import toast from "react-hot-toast";
 import { http } from "./axios.js"
 
+
 const APIServices = {};
 APIServices.adminLogin = async (body) => {
     try {
+        console.log("🚀 ~ http:", http)
         const res = await http.post('/login', body);
+        console.log("🚀 ~ APIServices.adminLogin= ~ res:", res)
         toast.success(res.data.message);
         return res
     } catch (err) {
         console.log('API call failed :-', err);
-        toast.error(err.response.data.message)
+        toast.error(err.response.data.message);
         throw err
     }
 }
